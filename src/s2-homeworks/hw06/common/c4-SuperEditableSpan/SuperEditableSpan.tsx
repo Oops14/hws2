4ec17df2d00d0aa42ba610b73da37f03fa73b6c4
Oops14 <ups14q@gmail.com>
2024-01-31 00:58:06 +0200

    homet

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -47,15 +47,15 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
     }
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
         // выключить editMode при нажатии за пределами инпута // делают студенты
-
-        onBlur?.(e)
+        onBlur?.(e);
+        setEditMode(false);
     }
     const onDoubleClickCallBack = (
         e: React.MouseEvent<HTMLSpanElement, MouseEvent>
     ) => {
         // включить editMode при двойном клике // делают студенты
-
         onDoubleClick?.(e)
+        setEditMode(true);
     }
 
     const spanClassName = s.span
